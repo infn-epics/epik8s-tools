b9d277e47bb28148b16680d02fc5710fe4109dd0
amichelo <andrea.michelotti@lnf.infn.it>
2024-11-12 00:15:27 +0100

    added template jnja

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="epik8s-tools",
-    version="0.4.3",
+    version="0.4.4",
     packages=find_packages(),
     include_package_data=True,  # Ensure to include files from MANIFEST.in
 
